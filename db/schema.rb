7f89712b21f03030c69ed1c53e46784e05723c68
yanyu <dcjcsk0205@gmail.com>
2020-03-02 15:45:57 +0900

    null false追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_065103) do
+ActiveRecord::Schema.define(version: 2020_03_02_063747) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -60,8 +60,8 @@ ActiveRecord::Schema.define(version: 2020_02_22_065103) do
   end
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
