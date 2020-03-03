6f0d63de90ded186b44b3ed68c173d71e15b33a4
yanyu <dcjcsk0205@gmail.com>
2020-03-04 00:29:08 +0900

    バリデーション追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,9 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
 
+  validates :title, presence: true
+  validates :body, { presence: true, length:{maximum: 255} }
+
   is_impressionable
   
 end
